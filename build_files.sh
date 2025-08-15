b35d3a3034bd246cf355475e9f686ab11841a116
Jorge Junior <56480032+jorgejunior618@users.noreply.github.com>
2025-08-15 11:14:35 -0300

    fix: ajuste no download de dependencias

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-echo "[BUILD PHASE]: Instalando django ..."
-python3 -m pip install django
-
-echo "[BUILD PHASE]: Instalando rest_framework ..."
-python3 -m pip install djangorestframework
-
-echo "[BUILD PHASE]: Instalando django-cors-headers ..."
-python3 -m pip install django-cors-headers
+echo "[BUILD PHASE]: Baixando dependencias ..."
+python3 -m pip install -r requirements.txt
 
 echo "[BUILD PHASE]: Migrando banco de dados..."
-# python3 manage.py makemigrations --noinput
-# python3 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "[BUILD PHASE]: Coletando arquivos estáticos..."
-# python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput --clear
