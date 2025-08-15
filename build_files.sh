58aa3156de80a316d87dd95713d14421aeed5603
Jorge Junior <56480032+jorgejunior618@users.noreply.github.com>
2025-08-15 11:02:19 -0300

    fix: pip3 não é reconhecido pelo vercel

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 echo "[BUILD PHASE]: Instalando django ..."
-python3 -m pip3 install django
+python3 -m pip install django
 
 echo "[BUILD PHASE]: Instalando rest_framework ..."
-python3 -m pip3 install djangorestframework
+python3 -m pip install djangorestframework
 
 echo "[BUILD PHASE]: Instalando django-cors-headers ..."
-python3 -m pip3 install django-cors-headers
+python3 -m pip install django-cors-headers
 
 echo "[BUILD PHASE]: Migrando banco de dados..."
 python3 manage.py makemigrations --noinput
