88443444c4429025e0eada3af1d1c1bbb618655a
Jorge Junior <56480032+jorgejunior618@users.noreply.github.com>
2025-08-15 10:58:23 -0300

    fix: instalando django na maquina remota

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Instalando django ..."
+python3 -m pip install django
+
 echo "Migrando banco de dados..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
