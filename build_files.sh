01954641a78a163685f5f38d52c63358d7ab4e9d
Jorge Junior <56480032+jorgejunior618@users.noreply.github.com>
2025-08-15 11:07:29 -0300

    fix: testando build sem efetuar a migraçao

--- build_files.sh
+++ build_files.sh
@@ -10,8 +10,8 @@ echo "[BUILD PHASE]: Instalando django-cors-headers ..."
 python3 -m pip install django-cors-headers
 
 echo "[BUILD PHASE]: Migrando banco de dados..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+# python3 manage.py makemigrations --noinput
+# python3 manage.py migrate --noinput
 
 echo "[BUILD PHASE]: Coletando arquivos estáticos..."
 python3 manage.py collectstatic --noinput
