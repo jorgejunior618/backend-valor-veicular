a040e8d0ddcc62d49cdfd9f3a205451815346e52
Jorge Junior <56480032+jorgejunior618@users.noreply.github.com>
2025-08-15 11:23:33 -0300

    feat: arquivos para publicação no vercel

--- backend/settings.py
+++ backend/settings.py
@@ -99,11 +99,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgresql',
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': '/var/www/mysite/sqlite.db',                      # Or path to database file if using sqlite3.
+#         'USER': '',                      # Not used with sqlite3.
+#         'PASSWORD': '',                  # Not used with sqlite3.
+#         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- build_files.sh
+++ build_files.sh
@@ -3,9 +3,9 @@
 echo "[BUILD PHASE]: Baixando dependencias ..."
 python3 -m pip install -r requirements.txt
 
-# echo "[BUILD PHASE]: Migrando banco de dados..."
-# python3 manage.py makemigrations --noinput
-# python3 manage.py migrate --noinput
+echo "[BUILD PHASE]: Migrando banco de dados..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 echo "[BUILD PHASE]: Coletando arquivos estáticos..."
 python3 manage.py collectstatic --noinput --clear
