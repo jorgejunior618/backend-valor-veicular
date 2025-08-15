614e30d3c220f5fa07fb857a6223f9e89e864024
Jorge Junior <56480032+jorgejunior618@users.noreply.github.com>
2025-08-15 11:41:31 -0300

    feat: arquivos para publicação no vercel

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,6 +75,7 @@ CORS_ALLOW_HEADERS = (
 )
 
 ROOT_URLCONF = 'backend.urls'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 TEMPLATES = [
     {
--- build_files.sh
+++ build_files.sh
@@ -5,7 +5,7 @@ python3 -m pip install -r requirements.txt
 
 echo "[BUILD PHASE]: Migrando banco de dados..."
 python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+# python3 manage.py migrate --noinput
 
 echo "[BUILD PHASE]: Coletando arquivos estáticos..."
 python3 manage.py collectstatic --noinput --clear
