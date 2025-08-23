ba5b6dbb1d908cb2d9d3eb3e6fa401a0d1ed2ed8
Jorge Junior <56480032+jorgejunior618@users.noreply.github.com>
2025-08-23 08:21:42 -0300

    fix: ajuste nos atributos obrigatorios

--- previsoes/serializers.py
+++ previsoes/serializers.py
@@ -9,7 +9,7 @@ class CarroSerializer(serializers.ModelSerializer):
   Condition = serializers.CharField(required=True, max_length=50)
   Year = serializers.IntegerField(required=True, min_value=2000)
   Mileage = serializers.IntegerField(required=True, min_value=0)
-  Options_Features = serializers.CharField(required=True, max_length=200)
+  Options_Features = serializers.CharField(required=False, max_length=200)
   Accident = serializers.CharField(required=True, max_length=4)
 
   class Meta:
